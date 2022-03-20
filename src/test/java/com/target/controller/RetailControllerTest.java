1441fce0bdcd4e5c014a0cb525208e4bcc69f143
Rajvi Mistry <rajvimistry@Rajvis-MBP.lan>
2022-03-20 01:09:11 -0500

    Adding negative scenario integration tests

--- src/test/java/com/target/controller/RetailControllerTest.java
+++ src/test/java/com/target/controller/RetailControllerTest.java
@@ -1,5 +1,6 @@
 package com.target.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -14,10 +15,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.target.dao.ProductDetailsRepository;
+import com.target.excpetion.RecordNotFoundException;
+import com.target.excpetion.ValidationException;
 import com.target.model.dto.ProductPrice;
 import com.target.myretail.BaseMvcTest;
 
@@ -55,6 +59,19 @@ class RetailControllerTest extends BaseMvcTest {
 	}
 	
 	@Test
+	public void getProductDetailsProductNotFound() throws Exception {
+
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.get("/products/54456118")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound())
+				.andReturn();
+		
+		Optional<RecordNotFoundException> e = Optional.ofNullable((RecordNotFoundException)result.getResolvedException());
+		e.ifPresent( (ex) -> assertThat(ex.getMessage().equals("Product 54456118 Not Found")));
+	}
+	
+	@Test
 	public void updateProductPriceSuccess() throws Exception {
 		ResultActions result = mockMvc.perform(
 				MockMvcRequestBuilders.put("/products/13860428")
@@ -70,8 +87,46 @@ class RetailControllerTest extends BaseMvcTest {
 			    .andExpect(jsonPath("$.current_price.currency_code").value("CAD"));;
 	}
 	
+	@Test
+	public void updateProductPriceBadRequest() throws Exception {
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.put("/products/13860429")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json)
+				.accept(MediaType.APPLICATION_JSON)
+				).andExpect(status().isBadRequest())
+				.andReturn();
+		
+		Optional<ValidationException> e = Optional.ofNullable((ValidationException)result.getResolvedException());
+		e.ifPresent( (ex) -> assertThat(ex.getMessage().equals("Invalid Payload")));
+	}
+	
+	@Test
+	public void updateProductPriceRecordNotFound() throws Exception {
+		when(productDetailsRepository.findById(13860428)).thenReturn(null);
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders.put("/products/13860429")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(wrong_product_id)
+				.accept(MediaType.APPLICATION_JSON)
+				).andExpect(status().isNotFound())
+				.andReturn();
+		
+		Optional<RecordNotFoundException> e = Optional.ofNullable((RecordNotFoundException)result.getResolvedException());
+		e.ifPresent( (ex) -> assertThat(ex.getMessage().equals("Product 54456118 Not Found")));
+	}
+	
 	private String json = "{\n" + 
 			"    \"id\": 13860428,\n" + 
+			"    \"name\": \"The Big Lebowski (Blu-ray)\",\n" + 
+			"    \"current_price\": {\n" + 
+			"        \"value\": 15.49,\n" + 
+			"        \"currency_code\": \"CAD\"\n" + 
+			"    }\n" + 
+			"}";
+	
+	private String wrong_product_id = "{\n" + 
+			"    \"id\": 13860429,\n" + 
 			"    \"name\": \"The Big Lebowski (Blu-ray)\",\n" + 
 			"    \"current_price\": {\n" + 
 			"        \"value\": 15.49,\n" + 
